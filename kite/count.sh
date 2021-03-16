5f73a33547123deaf43750510587bfd4621cb50f
Abolfazl (Abe) <53412130+abearab@users.noreply.github.com>
2021-03-17 01:12:26 +0330

    correct whitelist

--- kite/count.sh
+++ kite/count.sh
@@ -1,19 +1,17 @@
 fastq_R1=$1
 fastq_R2=$2
-outdir=$3
+Outdir=$3
+Threads=$4
 
-WL='/rumi/shams/abe/anaconda3/envs/alignment/lib/python3.6/site-packages/kb_python/whitelists/10xv2_whitelist.txt.gz'
-
-
-cd $outdir
+cd $Outdir
 
 ## 3. Run kallisto
 ## Pseudoalign the reads:
-kallisto bus -i $idx -o ./ -x 10xv2 -t 4 $fastq_R1 $fastq_R2
+kallisto bus -i $idx -o ./ -x 10xv2 -t $Threads $fastq_R1 $fastq_R2
 
 ## 4. Run bustools
 # For `bustools count`, use the mismatch t2g file.
-bustools correct -w $WL ./output.bus -o ./output_corrected.bus
+bustools correct -w ./10xv2_whitelist.txt ./output.bus -o ./output_corrected.bus
 
 bustools sort -t 4 -o ./output_sorted.bus ./output_corrected.bus
 
