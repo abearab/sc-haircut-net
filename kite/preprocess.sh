793ecd1f6e170d2d54319aaad5938e749ec385ae
abardesigner <abardesigner@gmail.com>
2021-03-17 22:28:20 +0000

    improve commands ussing kite
    opening an issue in kite to get help from pachterlab

--- kite/preprocess.sh
+++ kite/preprocess.sh
@@ -1,11 +1,15 @@
-FeaturesCSV=$1
+indir=$1
+FeaturesCSV=$2
 
 t2g=${FeaturesCSV/.csv/.Mismatch.t2g}
 fa=${FeaturesCSV/.csv/.Mismatch.fa}
 idx=${FeaturesCSV/.csv/.Mismatch.idx}
+kite='/rumi/shams/abe/Projects/sc-haircut-net/kite'
+
+cd $indir
 
 ## 1. kite pre-processing
-featuremap.py $FeaturesCSV --t2g FeaturesMismatch.t2g --fa ./FeaturesMismatch.fa --header --quiet
+python ${kite}/featuremap.py $FeaturesCSV --t2g $t2g --fa $fa --header --quiet
 
 ## 2. Build Index
 ## Build the kallisto index using the mismatch fasta and a k-mer length `-k` equal to the length of the Feature Barcodes:
