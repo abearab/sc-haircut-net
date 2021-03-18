ca632e27fcd0ea93541e34aafe6ec4b54a7c5c7b
abardesigner <abardesigner@gmail.com>
2021-03-18 14:45:44 +0000

    update haircut_pipeline

--- sc-haircut/haircut_pipeline/snakecharmer.sh
+++ sc-haircut/haircut_pipeline/snakecharmer.sh
@@ -1,37 +1,12 @@
-#!/usr/bin/env bash
 
-#BSUB -J haircut 
-#BSUB -o logs/snakemake_%J.out
-#BSUB -e logs/snakemake_%J.err
-#BSUB -R "select[mem>4] rusage[mem=4] " 
-#BSUB -q normal
-set -o nounset -o pipefail -o errexit -x
+args='
+  -q normal
+  -o {log}.out
+  -e {log}.err
+  -J {params.job_name}
+  -R "{params.memory} span[hosts=1] "
+  -n {threads} '
 
-args=' 
-  -q normal 
-  -o {log}.out 
-  -e {log}.err 
-  -J {params.job_name} 
-  -R "{params.memory} span[hosts=1] " 
-  -n {threads} ' 
-    
-
-#### load necessary programs ####
-
-# If programs are not all in the path then modify code to load 
-# the necessary programs
-
-# load modules
-. /usr/share/Modules/init/bash
-module load modules modules-init modules-python
-module load samtools/1.5
-module load bowtie2/2.3.2
-module load R/3.5.0
-module load cellranger/3.0.2
-
-# other programs (not in modules)
-# umi_tools 
-# cutadapt
 
 #### execute snakemake ####
 
