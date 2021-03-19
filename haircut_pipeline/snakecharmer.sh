85ec405c791cb179a07e2aaa79e84b922d4e4efa
abardesigner <abardesigner@gmail.com>
2021-03-19 17:48:33 +0000

    running snakemake
    there is an error, I must remake c++ modules

--- haircut_pipeline/snakecharmer.sh
+++ haircut_pipeline/snakecharmer.sh
@@ -1,20 +1,10 @@
-
-args='
-  -q normal
-  -o {log}.out
-  -e {log}.err
-  -J {params.job_name}
-  -R "{params.memory} span[hosts=1] "
-  -n {threads} '
-
-
 #### execute snakemake ####
 
-snakemake --drmaa "$args" \
+snakemake -p \
     --snakefile Snakefile \
     --jobs 8 \
     --resources all_threads=8 \
     --latency-wait 50 \
-    --rerun-incomplete  \
     --configfile config.yaml \
     --use-conda
+
